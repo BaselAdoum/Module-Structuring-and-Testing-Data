8e35777f8bbc751464c73aa54ebee43f18d73365
basel <adoumbasel@gmail.com>
2025-10-20 15:06:23 +0200

    const position is important

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,5 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
-// what's the error ?
-
-console.log(`I was born in ${cityOfBirth}`);
+// what's the error ? const should be declared before console.log()
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
+
