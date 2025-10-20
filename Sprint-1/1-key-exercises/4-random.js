8c3f3fdb3831a415563cfc33c1b798b5f981bd9e
basel <adoumbasel@gmail.com>
2025-10-20 11:11:33 +0200

    I worked on understanding random function and testing it

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -4,6 +4,16 @@ const maximum = 100;
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 
 // In this exercise, you will need to work out what num represents?
+    // num is a random integer between 1 and 100, inclusive.
+
 // Try breaking down the expression and using documentation to explain what it means
+    // Math.floor round down to lower integer
+    //Math.random generate a float from0 to 1 (1 not included)
+    //+ minimum is to be sure 0 is not a value from random
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+    console.log(num);
+        //26
+        //12
+        //89
+        
