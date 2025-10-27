81b35bfc37099660d476d1a5765925ae2ddfeacb
basel <adoumbasel@gmail.com>
2025-10-27 15:07:42 +0100

    I analyzed the code and fixed the error

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,15 +1,24 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> it will not run because .slice(1) will cut one index.
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
-}
+// function capitalise(str) {
+//   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+//   return str;
+// }
+// console.log(capitalise("adoum"))
 
 // =============> write your explanation here
-//
+// 0.js:8 this the line where the problem is happening 
+// THis is the actual line where the error is produced
+// SyntaxError: Identifier 'str' has already been declared 
+// This is the interpretation of the problem itself.
+
 // =============> write your new code here
-//
+function capitalise(str) {
+  let capitalised = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return capitalised;
+}
+console.log(capitalise("adoum"));
