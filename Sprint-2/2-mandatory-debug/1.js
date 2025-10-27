f1a19e5499d18409dd5f96f7a473a7a0dddd45b7
basel <adoumbasel@gmail.com>
2025-10-27 16:25:53 +0100

    code is analyze and corrected

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,17 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> we can not separate return from the a+ b. and there should be brackets around a+b. 
 
-function sum(a, b) {
-  return;
-  a + b;
-}
+// function sum(a, b) {
+//   return;
+//   a + b;
+// }
 
-console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+// console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
-// =============> write your explanation here
+// =============> as expected, the error was cause by seperating a+b from return and not put a brackets around the values.
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function sum(a, b) {
+  return (a + b);
+}
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
