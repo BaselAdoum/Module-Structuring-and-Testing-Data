e8cdee2dab5a39eec2980fa8068d3bd5586e48f6
basel <adoumbasel@gmail.com>
2025-10-27 16:20:45 +0100

    Code analyzed and corrected

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,14 +1,20 @@
 // Predict and explain first...
 
-// =============> write your prediction here
+// =============> in line 6 we should rather write return (a * b) instead of console.log...
 
+// function multiply(a, b) {
+//   console.log(a * b);
+// }
+
+// console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+
+// =============> write your explanation here
+// As predicted, the error was caused by puting console.log () inside the function's body
+// Finally, correct the code to fix the problem
+//  =============> write your new code here
 function multiply(a, b) {
-  console.log(a * b);
+  return (a * b);
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
-
-// =============> write your explanation here
-
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
+// The result of multiplying 10 and 32 is 320
