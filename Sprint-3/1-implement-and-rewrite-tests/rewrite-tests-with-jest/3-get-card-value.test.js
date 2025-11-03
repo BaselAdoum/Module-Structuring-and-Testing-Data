b49f874fab78dac7e5f072a2e728444faecbf86e
basel <adoumbasel@gmail.com>
2025-11-03 13:49:27 +0100

    Tested with Jest npm test

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -8,6 +8,23 @@ test("should return 11 for Ace of Spades", () => {
 });
 
 // Case 2: Handle Number Cards (2-10):
+
+test("should return number value for number cards (2-9)", () => {
+  const fiveofHearts = getCardValue("5♥");
+  expect(fiveofHearts).toEqual(5);
+});
 // Case 3: Handle Face Cards (J, Q, K):
+test("should return 10 for face cards (J, Q, K)", () => {
+  const jackOfClubs = getCardValue("J♣");
+  expect(jackOfClubs).toEqual(10);
+});
 // Case 4: Handle Ace (A):
+test("should return 10 for 10 card", () => {
+  const tenOfHearts = getCardValue("10♥");
+  expect(tenOfHearts).toEqual(10);
+});
 // Case 5: Handle Invalid Cards:
+test("should return 'Invalid card' for invalid card rank", () => {
+  const invalidCard = getCardValue("X♠");
+  expect(invalidCard).toEqual("Invalid card");
+});
