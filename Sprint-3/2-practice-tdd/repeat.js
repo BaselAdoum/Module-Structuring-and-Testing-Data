9d91cc350e26713b14f2c74dd585cd3c80a93cbf
basel <adoumbasel@gmail.com>
2025-11-03 17:33:21 +0100

    Codes tested and implemented

--- Sprint-3/2-practice-tdd/repeat.js
+++ Sprint-3/2-practice-tdd/repeat.js
@@ -1,5 +1,14 @@
-function repeat() {
-  return "hellohellohello";
-}
+function repeat(str, count) {
+  if (count < 0) {
+    return "Invalid count";
+  }
 
+  let result = "";
+
+  for (let i = 0; i < count; i++) {
+    result += str;
+  }
+
+  return result;
+}
 module.exports = repeat;
